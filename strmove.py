c11e9e1417267dff3f2cce9e3f740c899660a7e8
Astenik <106985461+Astenik@users.noreply.github.com>
2022-08-13 22:44:34 +0400

    Create strmove.py

---
+++ strmove.py
@@ -0,0 +1,17 @@
+def strmove(name:str, count:int):
+          ''' this function returns name 
+          string moved count units to the rigth.
+          name is  string and is the first argument of the function.
+          count is the integer ang is the second argument 
+          of the function.'''
+          if count >= len(name):
+                return name 
+          string = ''
+          n = len(name) - count
+          for ind in range(n, len(name)):
+                 string += name[ind]
+          for ind1 in range(n):
+                  string += name[ind1]
+          return string
+count = int(input("insert count: "))
+print(f"your string moved {count} times to the rigth is: {strmove('tenikas', count)}")
